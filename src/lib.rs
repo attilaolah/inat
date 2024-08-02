f2e92998e3a9ec9508cc2c7cdbfb2e11e5b66039
Attila Oláh <attilaolah@gmail.com>
2024-08-02 16:38:28 +0200

    Cleanup.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use reqwest::{
-    header::{HeaderMap, HeaderValue, ACCEPT},
-    Client, StatusCode, Url,
+    header::{HeaderMap, HeaderName, HeaderValue, ToStrError, ACCEPT, CONTENT_TYPE},
+    Client, Response, StatusCode, Url,
 };
 use serde::Deserialize;
 use thiserror::Error;
@@ -12,8 +12,17 @@ pub struct Api {
 
 #[derive(Error, Debug)]
 pub enum ApiError {
-    #[error("status {0}: {1}")]
+    #[error("bad status: {0}; {1}")]
     BadStatus(StatusCode, String),
+
+    #[error("missing header: {0}")]
+    MissingHeader(HeaderName),
+
+    #[error("bad header {0}: {0}")]
+    BadHeader(HeaderName, ToStrError),
+
+    #[error("bad content type: {0}")]
+    BadContentType(String),
 
     #[error(transparent)]
     BadUrl(#[from] url::ParseError),
@@ -39,34 +48,48 @@ impl Api {
             base_url: base_url.to_string(),
         })
     }
+
     pub async fn user(&self, user_id: &str) -> Result<String, ApiError> {
-        let url = format!("{}/users/{}", self.base_url, user_id);
-        let res = self.client.get(url.parse::<Url>()?).send().await?;
+        let url: Url = format!("{}/users/{}", self.base_url, user_id).parse()?;
+        let res = self.client.get(url).send().await?;
 
-        if res.status().is_success() {
-            let ct = res.headers().get("content-type");
-            match ct {
-                Some(val) => match val.to_str() {
-                    Ok(val) => println!("ct ok: {}", val),
-                    Err(err) => println!("ct err: {}", err),
-                },
-                None => println!("ct not set"),
-            };
-
-            let text = res.text().await?;
-            println!("{}", text);
-            Ok(text)
-        } else {
-            Err(ApiError::BadStatus(
-                res.status(),
-                extract_error(&res.text().await.unwrap_or("".to_string())),
-            ))
+        if !res.status().is_success() {
+            return Err(ApiError::bad_status(res).await);
         }
+        ensure_json(&res)?;
+
+        let text = res.text().await?;
+        Ok(text)
     }
 }
 
-fn extract_error(data: &str) -> String {
-    let res: ApiResponse =
-        serde_json::from_str(data).unwrap_or_else(|_| ApiResponse { error: None });
-    res.error.unwrap_or_else(|| data.to_string())
+impl ApiError {
+    async fn bad_status(res: Response) -> Self {
+        Self::BadStatus(res.status(), extract_error(res).await)
+    }
+}
+
+fn ensure_json(res: &Response) -> Result<(), ApiError> {
+    let ct = res
+        .headers()
+        .get(CONTENT_TYPE)
+        .ok_or(ApiError::MissingHeader(CONTENT_TYPE))?
+        .to_str()
+        .map_err(|err| ApiError::BadHeader(CONTENT_TYPE, err))?
+        .trim()
+        .to_lowercase();
+    let parts: Vec<&str> = ct.split(';').map(|part| part.trim()).collect();
+    if (parts.len() > 0 && parts[0] != "application/json")
+        || (parts.len() > 1 && parts[1] != "charset=utf-8")
+    {
+        return Err(ApiError::BadContentType(ct.to_string()));
+    }
+
+    Ok(())
+}
+
+async fn extract_error(res: Response) -> String {
+    let data = res.text().await.unwrap_or("".to_string());
+    let api_res: ApiResponse = serde_json::from_str(&data).unwrap_or(ApiResponse { error: None });
+    api_res.error.unwrap_or_else(|| data.to_string())
 }
